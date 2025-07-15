ea9d6d2ed18a039c19014085070155a966c28f4c
İlkay Genç <70265875+ilkaygenc@users.noreply.github.com>
2025-07-15 15:42:53 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ const (
 	apiURL      = "https://www.tesla.com/coinorder/api/v4/inventory-results?query=%7B%22query%22%3A%7B%22model%22%3A%22my%22%2C%22condition%22%3A%22new%22%2C%22options%22%3A%7B%7D%2C%22arrangeby%22%3A%22Price%22%2C%22order%22%3A%22asc%22%2C%22market%22%3A%22TR%22%2C%22language%22%3A%22tr%22%2C%22super_region%22%3A%22north%20america%22%2C%22lng%22%3A28.9533%2C%22lat%22%3A41.0145%2C%22zip%22%3A%2234096%22%2C%22range%22%3A0%7D%2C%22offset%22%3A0%2C%22count%22%3A24%2C%22outsideOffset%22%3A0%2C%22outsideSearch%22%3Afalse%2C%22isFalconDeliverySelectionEnabled%22%3Atrue%2C%22version%22%3A%22v2%22%7D"
 	botToken    = "8047920092:AAGDis_dQ1sjwopmR9MXXawrctPh4fNAZ4w"
 	chatID      = "8047920092"
-	checkPeriod = 60 * time.Second
+	checkPeriod = 6 * time.Second
 )
 
 var seen = make(map[string]bool)
